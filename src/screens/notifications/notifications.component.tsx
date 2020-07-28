f635a947e9b7e616673f274cc8619e88d1495958
Janith Gamage <janithjayasankha@gmail.com>
2020-07-28 16:19:30 +0100

    notifications screen init

--- src/screens/notifications/notifications.component.tsx
+++ src/screens/notifications/notifications.component.tsx
@@ -1,18 +1,11 @@
 import React from 'react';
-import { StyleSheet, ListRenderItemInfo, Dimensions } from 'react-native';
-import {
-  Divider,
-  List,
-  TopNavigation,
-  TopNavigationAction,
-  Text,
-  Layout,
-} from '@ui-kitten/components';
+import { ScrollView, StyleSheet, ListRenderItemInfo, View, ActivityIndicator } from 'react-native';
+import { Divider, List, TopNavigation, TopNavigationAction, Layout, Icon } from '@ui-kitten/components';
 import { SafeAreaLayout } from '../../components/safe-area-layout.component';
-import { MenuIcon } from '../../components/icons';
+import { MenuIcon, InfoIcon } from '../../components/icons';
 import { NotificationItem } from '../../components/notification.component';
 import { Notification } from '../../models/notification';
-import { ScrollView } from 'react-native';
+import { FirebaseService } from '../../services/firebase.service';
 
 const renderItem = (info: ListRenderItemInfo<Notification>): React.ReactElement => (
   <NotificationItem
@@ -26,14 +19,20 @@ export class NotificationsScreen extends React.Component {
 
   constructor(props) {
     super(props);
+    this.state = {
+      loading: true,
+      notifications: []
+    }
+    this.renderItem = this.renderItem.bind(this)
   }
 
-  initialMessages: Notification[] = [
-    Notification.first(),
-    Notification.second(),
-    Notification.third(),
-    Notification.fourth(),
-  ];
+  componentDidMount() {
+    FirebaseService.subscribeForNotifications(this.onSuccess);
+  }
+
+  onSuccess = (querySnapshot) => {
+    this.setState({notifications: querySnapshot.data().notifications.reverse(), loading: false});
+  }
 
   renderDrawerAction = (): React.ReactElement => (
     <TopNavigationAction
@@ -42,12 +41,21 @@ export class NotificationsScreen extends React.Component {
     />
   );
 
-  renderItem (info: ListRenderItemInfo<Notification>) {
-    <NotificationItem
-      style={styles.item}
-      notification={info.item}
-      /*onPress={onItemPress}*/
+  renderSOS = (): React.ReactElement => (
+    <TopNavigationAction
+        icon={InfoIcon}
+        onPress={() => this.props.navigation.navigate('Health')}
     />
+  );
+
+  renderItem (info: ListRenderItemInfo<Notification>) {
+    return(
+      <NotificationItem
+        style={styles.item}
+        notification={info.item}
+        /*onPress={onItemPress}*/
+      />
+    )
   }
 
   onItemPress() {
@@ -66,7 +74,30 @@ export class NotificationsScreen extends React.Component {
     </Layout>
   }*/
 
+  StarIcon = (style) => (
+    <Icon {...style} width={30} height={30} name='phone-call-outline'/>
+  );
+
   render() {
+    if (this.state.loading) {
+      return (
+        <SafeAreaLayout
+          style={styles.safeArea}
+          insets='top'>
+          <TopNavigation
+            title='Notifications'
+            leftControl={this.renderDrawerAction()}
+            rightControls={[this.renderSOS()]}
+          />
+          <Divider/>
+          <Layout>
+            <View style={styles.loading}>
+              <ActivityIndicator size='large' color="#712177" />
+            </View>
+          </Layout>
+        </SafeAreaLayout>
+      );
+    }
     return (
       <SafeAreaLayout
         style={styles.safeArea}
@@ -74,46 +105,48 @@ export class NotificationsScreen extends React.Component {
         <TopNavigation
           title='Notifications'
           leftControl={this.renderDrawerAction()}
+          rightControls={[this.renderSOS()]}
         />
-        
+        <Divider/>
         <Layout>
-          <Text style={styles.title}>Traxivity</Text>
           <ScrollView style={styles.container}>
             <List
               style={styles.list}
-              data={this.initialMessages}
-              renderItem={renderItem}
+              data={this.state.notifications}
+              renderItem={this.renderItem}
             />
           </ScrollView>
-          <Text style={styles.title}>Emotivity</Text>
-          <ScrollView style={styles.container}>
-            <List
-              style={styles.list}
-              data={this.initialMessages}
-              renderItem={renderItem}
-            />
-          </ScrollView>
+          {/*<Button style={styles.button} onPress={() => this.props.navigation.navigate('Health')} status='danger'>SOS</Button>*/}
         </Layout>
       </SafeAreaLayout>
     );
   }
-
 };
 
 const styles = StyleSheet.create({
   safeArea: {
     flex: 1,
   },
+  loading: {
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '100%',
+    marginHorizontal: 24
+  },
   container: {
-    height: (Dimensions.get('window').height/3)-10,
+    //height: (Dimensions.get('window').height/3)-10,
+    height: '100%'
   },
   list: {
     flex: 1
   },
   item: {
     paddingBottom: 10,
-    borderTopWidth: 1,
-    borderTopColor: '#EAF0F4',
+    //borderTopWidth: 1,
+    //borderTopColor: '#EAF0F4',
+    borderBottomWidth: 1,
+    borderBottomColor: '#EAF0F4',
     //borderBottomWidth: 1,
     //borderBottomColor: theme['background-basic-color-3'],
   },
@@ -125,5 +158,13 @@ const styles = StyleSheet.create({
     //borderBottomColor: '#ced5dd',
     borderTopWidth: 1,
     borderTopColor: '#ced5dd'
+  },
+  button: {
+    width: 80,
+    height: 80,
+    borderRadius: 50,
+    position: 'absolute',                                          
+    bottom: 80,                                                    
+    right: 30,
   }
 });
