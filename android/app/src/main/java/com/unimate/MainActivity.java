bd8a70cc1abd876e34afc1150cce0764c6f80c17
Janith Gamage <janithjayasankha@gmail.com>
2020-07-28 16:01:26 +0100

    android assets

--- android/app/src/main/java/com/unimate/MainActivity.java
+++ android/app/src/main/java/com/unimate/MainActivity.java
@@ -1,15 +1,52 @@
 package com.unimate;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.os.Bundle;
 import com.facebook.react.ReactActivity;
+import com.facebook.react.ReactActivityDelegate;
+import com.facebook.react.ReactRootView;
+import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
+import com.unimate.packages.splash.SplashScreen;
 
 public class MainActivity extends ReactActivity {
 
-  /**
-   * Returns the name of the main component registered from JavaScript. This is used to schedule
-   * rendering of the component.
-   */
+  @Override protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    SplashScreen.getInstance().open(this);
+  }
+
   @Override
-  protected String getMainComponentName() {
+  public void onConfigurationChanged(Configuration newConfig) {
+    super.onConfigurationChanged(newConfig);
+    Intent intent = new Intent("onConfigurationChanged");
+    intent.putExtra("newConfig", newConfig);
+    this.sendBroadcast(intent);
+  }
+
+  @Override
+  protected ReactActivityDelegate createReactActivityDelegate() {
+    return new ReactActivityDelegate(this, getMainComponentName()) {
+      @Override
+      protected ReactRootView createRootView() {
+        return new RNGestureHandlerEnabledRootView(MainActivity.this);
+      }
+    };
+  }
+
+  @Override protected void onDestroy() {
+    super.onDestroy();
+    SplashScreen.getInstance().reset();
+  }
+
+  @Override protected String getMainComponentName() {
     return "unimate";
   }
+
+  public static void start(Context context) {
+    Intent starter = new Intent(context, MainActivity.class);
+    context.startActivity(starter);
+  }
 }
+
