a88c0bf2a278087e416b9a8f5754fed2f9e49fa9
VihangaAW <vihanga.2017581@iit.ac.lk>
2021-09-26 12:06:17 +0530

    Formatted code

--- src/components/todo-input.component.tsx
+++ src/components/todo-input.component.tsx
@@ -1,27 +1,42 @@
 import React, {useState} from 'react';
 import {TouchableOpacity, View, TextInput, StyleSheet} from 'react-native';
 // import { Text } from '@ui-kitten/components';
-import {Icon, Text, Input, ListItem } from '@ui-kitten/components';
-
+import {Icon, Text, Input, ListItem} from '@ui-kitten/components';
 
 export const TodoInput = (props): React.ReactElement => {
   const [text, setText] = React.useState<string>('');
 
-
   return (
     <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
-    <Input
-    style={{ flex: 1, height: 40, fontSize: 14 }}
-    onChangeText={text => setText(text)}
-    value={text}
-    placeholder='Write a task...'
-    />
-    <TouchableOpacity
-    style={{marginLeft: 8, padding: 8, backgroundColor: '#712177', justifyContent: 'center', alignItems: 'center', borderRadius: 100}}
-    onPress={() => {props.onPress(text); setText("");}}>
-    <Text style={{color: '#fafafa', marginHorizontal:6, marginVertical:2, fontSize:20}}>+</Text>
-    </TouchableOpacity>
+      <Input
+        style={{flex: 1, height: 40, fontSize: 14}}
+        onChangeText={text => setText(text)}
+        value={text}
+        placeholder="Write a task..."
+      />
+      <TouchableOpacity
+        style={{
+          marginLeft: 8,
+          padding: 8,
+          backgroundColor: '#712177',
+          justifyContent: 'center',
+          alignItems: 'center',
+          borderRadius: 100,
+        }}
+        onPress={() => {
+          props.onPress(text);
+          setText('');
+        }}>
+        <Text
+          style={{
+            color: '#fafafa',
+            marginHorizontal: 6,
+            marginVertical: 2,
+            fontSize: 20,
+          }}>
+          +
+        </Text>
+      </TouchableOpacity>
     </View>
-
   );
 };
