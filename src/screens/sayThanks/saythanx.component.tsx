5694ff1f96956e499a1ce7777732ed5befe57d05
nadun.ys <yashnadun@gmail.com>
2021-12-03 21:36:50 +0530

    feat(search-user): Search user
    Co-authored-by: shalinieanne98 <shalinieanne98@users.noreply.github.com>

--- src/screens/sayThanks/saythanx.component.tsx
+++ src/screens/sayThanks/saythanx.component.tsx
@@ -1,18 +1,87 @@
+import {FirebaseAuthTypes} from '@react-native-firebase/auth';
 import {useNavigation} from '@react-navigation/core';
 import {
   Button,
+  Divider,
+  Icon,
+  Input,
+  List,
+  ListItem,
   TopNavigation,
   TopNavigationAction,
 } from '@ui-kitten/components';
-import React, {FC} from 'react';
-import {View, Text, StyleSheet} from 'react-native';
-import {InfoIcon, MenuIcon} from '../../components/icons';
+import React, {FC, useCallback, useEffect, useRef, useState} from 'react';
+import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
+import {User} from '../../models/auth/user';
+import {AppStorage} from '../../services/app-storage.service';
+import {InfoIcon, MenuIcon, SearchIcon} from '../../components/icons';
+import {FirebaseService} from '../../services/firebase.service';
+import {FirebaseFirestoreTypes} from '@react-native-firebase/firestore';
 interface SayThanksScreenProps {
   // props
 }
 
 export const SayThanksScreen: FC<SayThanksScreenProps> = () => {
+  const [searchText, setSearchText] = useState('');
+  const [filteredUsers, setFilteredUsers] = useState<User[] | null>(null);
+  const [users, setUsers] = useState<User[] | null>(null);
+  const [currentUser, setCurrentUser] = useState<User | null>(null);
+  const searchRef = useRef<any>();
+
   const nav = useNavigation();
+
+  const onGettingUsers = useCallback(
+    async (documents: FirebaseFirestoreTypes.QuerySnapshot) => {
+      if (documents.empty || !currentUser) {
+        return;
+      }
+
+      const userDocs = documents.docs
+        .map(doc => doc.data())
+        .map((value: any) => value.user)
+        .filter(user => user.uid !== currentUser.uid)
+        .map(doc => doc as User);
+
+      setUsers(userDocs);
+    },
+    [currentUser],
+  );
+
+  useEffect(() => {
+    (async () => {
+      const user = await AppStorage.getUser();
+      setCurrentUser(user);
+      await FirebaseService.getAllUsers(onGettingUsers);
+    })();
+  }, [onGettingUsers]);
+
+  useEffect(() => {
+    if (!searchRef.current.isFocused() || !users || users.length === 0) {
+      return;
+    }
+
+    if (searchText === '') {
+      setFilteredUsers(users);
+      return;
+    }
+
+    const filteredList = users.filter(
+      user =>
+        user.displayName.includes(searchText) ||
+        user.email.includes(searchText),
+    );
+    setFilteredUsers(filteredList);
+  }, [searchText, users]);
+
+  const renderItemIcon = props => <Icon {...props} name="person" />;
+
+  const renderItem = (props: {item: User; index: number}) => (
+    <ListItem
+      title={props.item.displayName}
+      description={props.item.email}
+      icon={renderItemIcon}
+    />
+  );
 
   const renderSOS = (): React.ReactElement => (
     <TopNavigationAction
@@ -37,6 +106,16 @@ export const SayThanksScreen: FC<SayThanksScreenProps> = () => {
       {/* <Divider/> */}
       <View style={styles.bar}>
         <View style={styles.container}>
+          <Input
+            style={{height: 40, fontSize: 14}}
+            placeholder="Search text"
+            icon={SearchIcon}
+            value={searchText}
+            onChange={nextValue => setSearchText(nextValue.nativeEvent.text)}
+            ref={searchRef}
+          />
+          <Divider />
+          <List data={filteredUsers} renderItem={renderItem} />
           <Text>Say Thank</Text>
           <Button
             onPress={() => {
