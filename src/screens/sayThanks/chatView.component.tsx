01b16b7f93da2bd3e509e0e19b4310b4c34f1219
nadunys <yashnadu@gmail.com>
2021-12-04 09:28:19 +0530

    sometihng i did

--- src/screens/sayThanks/chatView.component.tsx
+++ src/screens/sayThanks/chatView.component.tsx
@@ -1,11 +1,11 @@
-import {useNavigationState} from '@react-navigation/core';
-import React, {FC, useEffect, useState} from 'react';
-import {Button, Text, View} from 'react-native';
-import {FirebaseService} from '../../services/firebase.service';
+import { useNavigationState } from '@react-navigation/core';
+import React, { FC, useEffect, useState } from 'react';
+import { Button, Text, View } from 'react-native';
+import { FirebaseService } from '../../services/firebase.service';
 import { firebase } from "@react-native-firebase/firestore";
-import {GiftedChat} from 'react-native-gifted-chat';
+import { GiftedChat } from 'react-native-gifted-chat';
 import { AppStorage } from '../../services/app-storage.service'
-import { Input, Card } from '@ui-kitten/components';
+import { Input, Card, TopNavigation } from '@ui-kitten/components';
 import { StoredThanxMessage, ThanxMessage } from 'src/models/ThanxMessage';
 // import {FirebaseService} from './../../services/firebase.service';
 /**import { AppStorage } from './../../services/app-storage.service';
@@ -13,16 +13,16 @@ import { StoredThanxMessage, ThanxMessage } from 'src/models/ThanxMessage';
  * - SayThanx chat of a certain friend
  *
  */
-interface ChatScreenProps {}
-const messageMapper = (msg:StoredThanxMessage,toUID:string):ThanxMessage=>{
+interface ChatScreenProps { }
+const messageMapper = (msg: StoredThanxMessage, toUID: string): ThanxMessage => {
   return {
-    _id:1,
-    text:msg.text,
+    _id: 1,
+    text: msg.text,
     createdAt: new Date(msg.timeStamp),
-    user:{
-      _id:msg.form,
-      avatar:"s",
-      name:"Friend"
+    user: {
+      _id: msg.form,
+      avatar: "s",
+      name: "Friend"
     }
   }
 }
@@ -30,93 +30,100 @@ const messageMapper = (msg:StoredThanxMessage,toUID:string):ThanxMessage=>{
 // const mapMessages = (msgs:StoredThanxMessage[]):ThanxMessage[]=>msgs.map(messageMapper)
 
 const ChatView: FC<ChatScreenProps> = () => {
-  const {uid} = AppStorage.getUser();
+  const { uid, displayName } = AppStorage.getUser();
   const [msg, setmsg] = useState<ThanxMessage[]>([
-  
+
   ]);
 
-  const params = useNavigationState<{userId: string} | undefined>(
+  const params = useNavigationState<{ userId: string } | undefined>(
     state => state.routes[state.index].params,
   );
 
-    useEffect(()=>{
-     const unsub =  FirebaseService.setChatListener((doc)=>{
-        // user doc changed
-        console.log("user doc changed!!");
-        fetchChats();
-      })
+  useEffect(() => {
+    const unsub = FirebaseService.setChatListener((doc) => {
+      // user doc changed
+      console.log("user doc changed!!");
+      fetchChats();
+    })
 
-      return unsub
-    },[])
+    return unsub
+  }, [])
 
-    const fetchChats =async ()=>{
-      if(!params?.userId) {
-        console.log("NO chatUID!",{params});
-          
-        return null;
-      }
-      if(!params.userId){
-        console.log("No to userId");
-        
-        return null
-      }
-      
-      const sent = await FirebaseService.getSentChatsByReceiverId(params.userId);
-      const received = await FirebaseService.getReceivedChats(params.userId);
+  const fetchChats = async () => {
+    if (!params?.userId) {
+      console.log("NO chatUID!", { params });
 
-
-      if(sent && received ){
-        if(received.length == 1 && sent.lenth > 0 ){
-
-        }
-      }
-     
-      console.log("sent",{sent,received})
-      
-      setmsg(sortMessages([...sent,...received]))
+      return null;
     }
-    const sortMessages = (messages:ThanxMessage[])=>{
-      const sortedMessages = messages.map(message => {
-        const time = message.createdAt.toDate();
-        return ({...message, createdAt: time})
-      }).sort((a, b) => b.createdAt - a.createdAt);
-      console.log({sortedMessages});
-      return GiftedChat.append([],sortedMessages);
+    if (!params.userId) {
+      console.log("No to userId");
+
+      return null
     }
+
+    const sent = await FirebaseService.getSentChatsByReceiverId(params.userId);
+    const received = await FirebaseService.getReceivedChats(params.userId);
+
+
+    if (sent && received) {
+      if (received.length == 1 && sent.lenth > 0) {
+
+      }
+    }
+
+    console.log("sent", { sent, received })
+
+    setmsg(sortMessages([...sent, ...received]))
+  }
+  const sortMessages = (messages: ThanxMessage[]) => {
+    const sortedMessages = messages.map(message => {
+      const time = message.createdAt.toDate();
+      return ({ ...message, createdAt: time })
+    }).sort((a, b) => b.createdAt - a.createdAt);
+    console.log({ sortedMessages });
+    return GiftedChat.append([], sortedMessages);
+  }
 
   useEffect(() => {
     fetchChats();
   }, []);
 
-    // useEffect(() => {
-    //   if (params) {
-    //     setmsg(prev => ({...prev, toUID: params.chatUID}));
-    //   }
-    // }, [params]);
+  // useEffect(() => {
+  //   if (params) {
+  //     setmsg(prev => ({...prev, toUID: params.chatUID}));
+  //   }
+  // }, [params]);
 
   const handleMsgSend = ([msg]: ThanxMessage[]) => {
 
-    const message = {...msg, _id:params?.userId||msg[0]._id ,
-      createdAt:firebase.firestore.Timestamp.fromDate(msg.createdAt) }
+    const message = {
+      ...msg, _id: params?.userId || msg[0]._id,
+      createdAt: firebase.firestore.Timestamp.fromDate(msg.createdAt)
+    }
 
-    console.log('send msg', {message});
+    console.log('send msg', { message });
 
-    const sendMsg = async (message:ThanxMessage) =>{
-      if(!params?.userId) return null;
+    const sendMsg = async (message: ThanxMessage) => {
+      if (!params?.userId) return null;
       FirebaseService.sendChat(message)
     }
 
     sendMsg(message)
-    
-    setmsg(prev => GiftedChat.append(prev,[{...message, createdAt: message.createdAt.toDate()}]));
+
+    setmsg(prev => GiftedChat.append(prev, [{ ...message, createdAt: message.createdAt.toDate() }]));
 
 
   };
 
   return (
-    <View style={{flex: 1, backgroundColor: 'white'}}>
+    <View style={{ flex: 1, backgroundColor: 'white' }}>
+      <TopNavigation
+        title={displayName}
+        titleStyle={{ color: 'white' }}
+        style={{ backgroundColor: '#712177' }}
+      />
       <GiftedChat
-    
+
         // renderChatEmpty={
         // ()=>  <View>
         //     <Text>Yay! This is your first Thank to {params?.userId}</Text>
@@ -138,9 +145,9 @@ const ChatView: FC<ChatScreenProps> = () => {
         //     }} />
         //   </View>
         // }
-        messages={msg.map((el,i)=>{
+        messages={msg.map((el, i) => {
           // TODO handle date 
-          return{...el,_id:i}
+          return { ...el, _id: i }
 
         })}
         onSend={handleMsgSend}
